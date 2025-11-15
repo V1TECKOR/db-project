9a427659a80ecf764ea0e8b303b09cd2726cb1ee
Luca Egli <luca.egli@mng.ch>
2025-11-15 15:08:01 +0100

    Debugging

--- db.py
+++ db.py
@@ -17,16 +17,6 @@ def get_conn():
     return pool.get_connection()
 
 # DB-Helper
-def db_write(sql, params=None):
-    conn = get_conn()
-    try:
-        cur = conn.cursor()
-        cur.execute(sql, params or ())
-        conn.commit()
-    finally:
-        cur.close()
-        conn.close()
-
 def db_read(sql, params=None, single=False):
     conn = get_conn()
     try:
@@ -34,12 +24,31 @@ def db_read(sql, params=None, single=False):
         cur.execute(sql, params or ())
 
         if single:
+            # liefert EIN Dict oder None
             row = cur.fetchone()
+            print("db_read(single=True) ->", row)   # DEBUG
             return row
         else:
+            # liefert Liste von Dicts (evtl. [])
             rows = cur.fetchall()
+            print("db_read(single=False) ->", rows)  # DEBUG
             return rows
 
+    finally:
+        try:
+            cur.close()
+        except:
+            pass
+        conn.close()
+
+
+def db_write(sql, params=None):
+    conn = get_conn()
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, params or ())
+        conn.commit()
+        print("db_write OK:", sql, params)  # DEBUG
     finally:
         try:
             cur.close()
